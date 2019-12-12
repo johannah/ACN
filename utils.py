b409cfd951fd620b058f6233151da4490940f74b
johannah <jh1736@gmail.com>
2019-12-12 15:33:12 -0500

    changed pixel cnn conventions

--- utils.py
+++ utils.py
@@ -16,7 +16,7 @@ from IPython import embed
 
 class IndexedDataset(Dataset):
     def __init__(self, dataset_function, path, train=True, download=True, transform=transforms.ToTensor()):
-        """ class to provide indexes into the data
+        """ class to provide indexes into the data -- needed for ACN prior
         """
         self.indexed_dataset = dataset_function(path,
                              download=download,
@@ -35,16 +35,16 @@ def save_checkpoint(state, filename='model.pt'):
     torch.save(state, filename)
     print("finished save of model %s" %filename)
 
-def create_mnist_datasets(dataset_name, base_datadir, batch_size):
+def create_mnist_datasets(dataset_name, base_datadir, batch_size, dataset_transforms):
     dataset = eval('datasets.'+dataset_name)
     datadir = os.path.join(base_datadir, dataset_name)
     train_data = IndexedDataset(dataset, path=datadir,
                                 train=True, download=True,
-                                transform=transforms.ToTensor())
+                                transform=dataset_transforms)
     train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
     valid_data = IndexedDataset(dataset, path=datadir,
                                train=False, download=True,
-                               transform=transforms.ToTensor())
+                               transform=dataset_transforms)
     valid_loader = DataLoader(valid_data, batch_size=batch_size, shuffle=True)
     data_dict = {'train':train_loader, 'valid':valid_loader}
     nchans,hsize,wsize = data_dict['train'].dataset[0][0].shape
@@ -103,7 +103,7 @@ def rolling_average(a, n=5) :
     return ret[n - 1:] / n
 
 def plot_losses(train_cnts, train_losses, test_losses, name='loss_example.png', rolling_length=4):
-    f,ax=plt.subplots(1,1,figsize=(3,3))
+    f,ax=plt.subplots(1,1,figsize=(4,6))
     cmap = matplotlib.cm.get_cmap('viridis')
     color_idxs = np.linspace(.1,.9,num=len(train_losses.keys()))
     colors = np.array([cmap(ci) for ci in color_idxs])
@@ -197,4 +197,155 @@ def kl_loss_function(u_q, s_q, u_p, s_p):
      '''
     acn_KLD = (s_p-s_q-0.5 + ((2*s_q).exp() + (u_q-u_p).pow(2)) / (2*(2*s_p).exp()))
     return acn_KLD
+
+def discretized_mix_logistic_loss(prediction, target, nr_mix=10):
+    """ log-likelihood for mixture of discretized logistics, assumes the data has been rescaled to [-1,1] interval
+    Args:
+        prediction: model prediction. channels of model prediction should be mean
+                    and scale for each channel and weighting bt components --> (2*nr_mix+nr_mix)*num_channels
+        target: min/max should be -1 and 1
+    **** code for this function from https://github.com/pclucas14/pixel-cnn-pp/blob/master/utils.py
+    """
+    #assert (prediction.max()<=1 and prediction.min()>=-1)
+    assert (target.max()<=1 and target.min()>=-1)
+    device = target.device
+    # Pytorch ordering
+    l = prediction
+    x = target
+    x = x.permute(0, 2, 3, 1)
+    l = l.permute(0, 2, 3, 1)
+    xs = [int(y) for y in x.size()]
+    #ls = [int(y) for y in l.size()]
+
+    # here and below: unpacking the params of the mixture of logistics
+    #nr_mix = int(ls[-1] / 10)
+    # l is prediction
+    logit_probs = l[:, :, :, :nr_mix]
+
+    l = l[:, :, :, nr_mix:].contiguous().view(xs + [nr_mix*2]) # 3--changed to 1 for mean, scale, coef
+    means = l[:, :, :, :, :nr_mix]
+    log_scales = torch.clamp(l[:, :, :, :, nr_mix:2 * nr_mix], min=-7.)
+
+    # here and below: getting the means and adjusting them based on preceding
+    # sub-pixels
+    x = x.contiguous()
+    #x = x.unsqueeze(-1) + torch.Variable(torch.zeros(xs + [nr_mix]).to(device), requires_grad=False)
+    x = x.unsqueeze(-1) + torch.zeros(xs + [nr_mix], requires_grad=False).to(device)
+
+    centered_x = x - means
+    inv_stdv = torch.exp(-log_scales)
+    plus_in = inv_stdv * (centered_x + 1. / 255.)
+    cdf_plus = torch.sigmoid(plus_in)
+    min_in = inv_stdv * (centered_x - 1. / 255.)
+    cdf_min = torch.sigmoid(min_in)
+    # log probability for edge case of 0 (before scaling)
+    log_cdf_plus = plus_in - F.softplus(plus_in)
+    # log probability for edge case of 255 (before scaling)
+    log_one_minus_cdf_min = -F.softplus(min_in)
+    cdf_delta = cdf_plus - cdf_min  # probability for all other cases
+    mid_in = inv_stdv * centered_x
+    # log probability in the center of the bin, to be used in extreme cases
+    # (not actually used in our code)
+    log_pdf_mid = mid_in - log_scales - 2. * F.softplus(mid_in)
+
+    # now select the right output: left edge case, right edge case, normal
+    # case, extremely low prob case (doesn't actually happen for us)
+
+    # this is what we are really doing, but using the robust version below for extreme cases in other applications and to avoid NaN issue with tf.select()
+    # log_probs = tf.select(x < -0.999, log_cdf_plus, tf.select(x > 0.999, log_one_minus_cdf_min, tf.log(cdf_delta)))
+
+    # robust version, that still works if probabilities are below 1e-5 (which never happens in our code)
+    # tensorflow backpropagates through tf.select() by multiplying with zero instead of selecting: this requires use to use some ugly tricks to avoid potential NaNs
+    # the 1e-12 in tf.maximum(cdf_delta, 1e-12) is never actually used as output, it's purely there to get around the tf.select() gradient issue
+    # if the probability on a sub-pixel is below 1e-5, we use an approximation
+    # based on the assumption that the log-density is constant in the bin of
+    # the observed sub-pixel value
+
+    inner_inner_cond = (cdf_delta > 1e-5).float()
+    inner_inner_out  = inner_inner_cond * torch.log(torch.clamp(cdf_delta, min=1e-12)) + (1. - inner_inner_cond) * (log_pdf_mid - np.log(127.5))
+    inner_cond       = (x > 0.999).float()
+    inner_out        = inner_cond * log_one_minus_cdf_min + (1. - inner_cond) * inner_inner_out
+    cond             = (x < -0.999).float()
+    log_probs        = cond * log_cdf_plus + (1. - cond) * inner_out
+    log_probs        = torch.sum(log_probs, dim=3) + log_prob_from_logits(logit_probs)
+    lse = log_sum_exp(log_probs)
+    return -lse.mean()
+
+def to_one_hot(tensor, n, fill_with=1.):
+    # we perform one hot encore with respect to the last axis
+    one_hot = torch.FloatTensor(tensor.size() + (n,)).zero_()
+    if tensor.is_cuda : one_hot = one_hot.cuda()
+    one_hot.scatter_(len(tensor.size()), tensor.unsqueeze(-1), fill_with)
+    return one_hot
+
+def sample_from_discretized_mix_logistic(l, nr_mix, only_mean=True, deterministic=False):
+    """
+    TODO  explain input
+    **** code for this function from https://github.com/pclucas14/pixel-cnn-pp/blob/master/utils.py
+    l should be bt -1 and 1
+    """
+    print(l.min(), l.max())
+    # Pytorch ordering
+    l = l.permute(0, 2, 3, 1)
+    ls = [int(y) for y in l.size()]
+    xs = ls[:-1] + [1]
+
+    # unpack parameters
+    logit_probs = l[:, :, :, :nr_mix]
+    #from IPython import embed; embed()
+    l = l[:, :, :, nr_mix:].contiguous().view(xs + [nr_mix * 2])
+    # sample mixture indicator from softmax
+    temp = torch.FloatTensor(logit_probs.size())
+    if l.is_cuda : temp = temp.cuda()
+    temp.uniform_(1e-5, 1. - 1e-5)
+    # hack to make deterministic JRH
+    # could also just take argmax of logit_probs
+    if deterministic:
+        temp = temp*0.0+0.5
+    temp = logit_probs.data - torch.log(- torch.log(temp))
+    _, argmax = temp.max(dim=3)
+
+    one_hot = to_one_hot(argmax, nr_mix)
+    sel = one_hot.view(xs[:-1] + [1, nr_mix])
+    # select logistic parameters
+    means = torch.sum(l[:, :, :, :, :nr_mix] * sel, dim=4)
+    log_scales = torch.clamp(torch.sum(
+        l[:, :, :, :, nr_mix:2 * nr_mix] * sel, dim=4), min=-7.)
+    # sample from logistic & clip to interval
+    # we don't actually round to the nearest 8bit value when sampling
+    u = torch.FloatTensor(means.size())
+    if l.is_cuda : u = u.cuda()
+    u.uniform_(1e-5, 1. - 1e-5)
+    # hack to make deterministic
+    if deterministic:
+        u= u*0.0+0.5
+    #u = torch.Variable(u)
+    if only_mean:
+        x = means
+    else:
+        x = means + torch.exp(log_scales) * (torch.log(u) - torch.log(1. - u))
+    out = torch.clamp(torch.clamp(x,min=-1.),max=1.)
+    # put back in Pytorch ordering
+    out = out.permute(0, 3, 1, 2)
+    return out
+
+def log_sum_exp(x):
+    """ numerically stable log_sum_exp implementation that prevents overflow
+    **** code for this function from https://github.com/pclucas14/pixel-cnn-pp/blob/master/utils.py
+    """
+    # TF ordering
+    axis  = len(x.size()) - 1
+    m, _  = torch.max(x, dim=axis)
+    m2, _ = torch.max(x, dim=axis, keepdim=True)
+    return m + torch.log(torch.sum(torch.exp(x - m2), dim=axis))
+
+def log_prob_from_logits(x):
+    """ numerically stable log_softmax implementation that prevents overflow
+    **** code for this function from https://github.com/pclucas14/pixel-cnn-pp/blob/master/utils.py
+    """
+    # TF ordering
+    axis = len(x.size()) - 1
+    m, _ = torch.max(x, dim=axis, keepdim=True)
+    return x - m - torch.log(torch.sum(torch.exp(x - m), dim=axis, keepdim=True))
+
 
