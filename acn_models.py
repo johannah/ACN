2c41371230c5e97c4ba8c11fb1f126eb27544adc
johannah <jh1736@gmail.com>
2019-12-24 18:24:44 -0500

    acutally fixed vqvae

--- acn_models.py
+++ acn_models.py
@@ -300,57 +300,46 @@ class ConvEncodeDecodeLargeVQVAE(nn.Module):
         super(ConvEncodeDecodeLargeVQVAE, self).__init__()
         self.code_len = code_len
         self.encoder_output_size = encoder_output_size
-        # find reshape to match encoder --> eo is 4 with mnist (28,28)  and
-        # code_len of 64
-        self.eo = np.sqrt(encoder_output_size/(2*code_len))
+        self.num_clusters = num_clusters
+        self.num_z = num_z
+        self.eo = np.sqrt(encoder_output_size/code_len)
         assert self.eo == int(self.eo)
         self.eo = int(self.eo)
-
+        # code_len of 64
+        n = 16
         # architecture dependent
         self.encoder = nn.Sequential(
             nn.Conv2d(in_channels=input_size,
-                      out_channels=16,
+                      out_channels=n,
                       kernel_size=4,
                       stride=2, padding=1),
-            nn.BatchNorm2d(16),
+            nn.BatchNorm2d(n),
             nn.ReLU(True),
-            nn.Conv2d(in_channels=16,
-                      out_channels=16,
+            nn.Conv2d(in_channels=n,
+                      out_channels=n,
                       kernel_size=4,
                       stride=2, padding=1),
-            nn.BatchNorm2d(16),
+            nn.BatchNorm2d(n),
             nn.ReLU(True),
-            nn.Conv2d(in_channels=16,
-                      out_channels=16,
+            nn.Conv2d(in_channels=n,
+                      out_channels=n,
                       kernel_size=2,
                       stride=2, padding=1),
-            nn.BatchNorm2d(16),
+            nn.BatchNorm2d(n),
             nn.ReLU(True),
-            nn.Conv2d(in_channels=16,
-                      out_channels=code_len*2,
+            nn.Conv2d(in_channels=n,
+                      out_channels=num_z,
                       kernel_size=1,
                       stride=1, padding=0),
-            nn.BatchNorm2d(code_len*2),
+            nn.BatchNorm2d(num_z),
             nn.ReLU(True),
            )
-        # found via experimentation
-        n = 16
+
         self.fc21 = nn.Linear(encoder_output_size, code_len)
         self.fc22 = nn.Linear(encoder_output_size, code_len)
         self.fc3 = nn.Linear(code_len, encoder_output_size)
-        self.out_layer = nn.ConvTranspose2d(in_channels=n,
-                         out_channels=output_size,
-                         kernel_size=1,
-                         stride=1, padding=0)
-
-        # set bias to 0.5 for sigmoid with bce - 0 when using dml
-        self.out_layer.bias.data.fill_(last_layer_bias)
-        ## vq embedding scheme
-        self.embedding = nn.Embedding(num_clusters, num_z)
-        # common scaling for embeddings - variance roughly scales with num_clusters
-        self.embedding.weight.data.copy_(1./num_clusters * torch.randn(num_clusters, num_z))
         self.conv_layers = nn.Sequential(
-                               nn.ConvTranspose2d(in_channels=code_len*2,
+                               nn.ConvTranspose2d(in_channels=self.code_len,
                                   out_channels=num_z,
                                   kernel_size=1,
                                   stride=1, padding=0),
@@ -361,6 +350,20 @@ class ConvEncodeDecodeLargeVQVAE(nn.Module):
                                   kernel_size=1,
                                   stride=1, padding=0),
                                )
+
+        self.out_layer = nn.ConvTranspose2d(in_channels=n,
+                         out_channels=output_size,
+                         kernel_size=1,
+                         stride=1, padding=0)
+
+        # set bias to 0.5 for sigmoid with bce - 0 when using dml
+        self.out_layer.bias.data.fill_(last_layer_bias)
+        ## vq embedding scheme
+        #self.embedding = nn.Embedding(num_clusters, num_z)
+        # common scaling for embeddings - variance roughly scales with num_clusters
+        #self.embedding.weight.data.copy_(1./num_clusters * torch.randn(num_clusters, num_z))
+        # from Rithesh
+        self.codebook = VQEmbedding(num_clusters, num_z)
 
         self.decoder = nn.Sequential(
                 # 4x4
@@ -382,7 +385,7 @@ class ConvEncodeDecodeLargeVQVAE(nn.Module):
                          out_channels=n,
                          kernel_size=2,
                          stride=2, padding=1),
-                 nn.BatchNorm2d(16),
+                 nn.BatchNorm2d(n),
                  nn.ReLU(True),
                  # 14->28
                  nn.ConvTranspose2d(in_channels=n,
@@ -393,11 +396,7 @@ class ConvEncodeDecodeLargeVQVAE(nn.Module):
                  nn.ReLU(True),
                 self.out_layer,
                 )
-
-    def encode(self, x):
-        o = self.encoder(x)
-        ol = o.view(o.shape[0], o.shape[1]*o.shape[2]*o.shape[3])
-        return self.fc21(ol), self.fc22(ol)
+        self.apply(weights_init)
 
     def reparameterize(self, mu, logvar):
         if self.training:
@@ -408,57 +407,33 @@ class ConvEncodeDecodeLargeVQVAE(nn.Module):
         else:
             return mu
 
-    def forward(self, x):
-        z,mu,logvar = self.acn_encode(x)
-        x_tilde, z_e_x, z_q_x, latents = self.vq_decode(z)
-        return x_tilde, z, mu, logvar, z_e_x, z_q_x, latents
-
     def acn_encode(self, x):
-        # given input image - find acn, then vq
-        mu, logvar = self.encode(x)
+        o = self.encoder(x)
+        ol = o.view(o.shape[0], o.shape[1]*o.shape[2]*o.shape[3])
+        mu = self.fc21(ol)
+        logvar =self.fc22(ol)
         z = self.reparameterize(mu, logvar)
         return z, mu, logvar
 
-    def vq_decode(self, z):
+    def vq_encode(self, z):
+        # convert z to image shape
         co = F.relu(self.fc3(z))
-        # col is not bs,cl*s2,4,4 for mnist
-        col = co.view(co.shape[0], self.code_len*2, self.eo, self.eo)
-        # get continuous output directly from encoder
-        z_e_x = self.conv_layers(col)
-        # NCHW is the order in the encoder
-        # (num, channels, height, width)
-        N, C, H, W = z_e_x.shape
-        # need NHWC instead of default NCHW for easier computations
-        z_e_x_transposed = z_e_x.permute(0,2,3,1)
-        # needs C,K
-        emb = self.embedding.weight.transpose(0,1)
-        # broadcast to determine distance from encoder output to clusters
-        # NHWC -> NHWCK
-        measure = z_e_x_transposed.unsqueeze(4) - emb[None, None, None]
-        # num_clusters=512, num_z=64
-        # measure is of shape bs,10,10,64,512
-        # square each element, then sum over channels
-        # take sum over each z - find min
-        dists = torch.pow(measure, 2).sum(-2)
-        # pytorch gives real min and arg min - select argmin
-        # this is the closest k for each sample - Equation 1
-        # latents is a array of integers
-        # mnist latents are size bs,4,4
-        latents = dists.min(-1)[1]
-        # look up cluster centers
-        x_tilde, z_q_x = self.decode_clusters(latents, N, H, W, C)
-        return x_tilde, z_e_x, z_q_x, latents
+        co = co.view(z.shape[0], self.code_len, self.eo, self.eo)
+        z_e_x = self.conv_layers(co)
+        latents = self.codebook(z_e_x)
+        return z_e_x, latents
 
-    def decode_clusters(self, latents, N, H, W, C):
-        z_q_x = self.embedding(latents.view(latents.shape[0], -1))
-        # back to NCHW (orig) - now cluster centers/class
-        z_q_x = z_q_x.view(N, H, W, C).permute(0, 3, 1, 2)
-        # put quantized data through decoder
+    def decode(self, latents):
+        z_q_x = self.codebook.embedding(latents).permute(0, 3, 1, 2)  # (B, D, H, W)
         x_tilde = self.decoder(z_q_x)
-        # Move prediction to the z_q_x from z_e_x so that I can decode forward
-        # can predict value or reward
-        #return x_tilde, z_q_x, action, reward
         return x_tilde, z_q_x
+
+    def forward(self, x):
+        z, mu, logvar = self.acn_encode(x)
+        z_e_x, latents = self.vq_encode(z)
+        z_q_x_st, z_q_x = self.codebook.straight_through(z_e_x)
+        x_tilde = self.decoder(z_q_x_st)
+        return x_tilde, z, mu, logvar, z_e_x, z_q_x, latents
 
 class VQEmbedding(nn.Module):
     def __init__(self, K, D):
@@ -489,6 +464,8 @@ class VQVAE(nn.Module):
                        encoder_output_size=1000, last_layer_bias=0.5, num_clusters=512, num_z=32):
         super(VQVAE, self).__init__()
         self.encoder_output_size = encoder_output_size
+        self.num_clusters = num_clusters
+        self.num_z = num_z
         n = 16
         # architecture dependent
         self.encoder = nn.Sequential(
